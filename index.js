0a5b7d11303e707f812a689c694a4b410543e5e5
Magrishb <bmagrish@gmail.com>
2020-09-09 22:24:36 -0500

    Set variables for all id's in my JS page.

---
+++ index.js
@@ -0,0 +1,13 @@
+// List all the variables.
+var timer = document.getElementById("timer");
+var startingContainer = document.getElementById("starting-continger")
+var startButton = document.getElementById("start-quiz-btn");
+var questionContainer = document.getElementById("question-container");
+var title = document.getElementById("title");
+var ansA = document.getElementById("A");
+var ansB = document.getElementById("A");
+var ansC = document.getElementById("A");
+var ansD = document.getElementById("A");
+var submitForm = document.getElementById("submit-form");
+var submitEmail = document.getElementById("email-input");
+var submitButton = document.getElementById("submit-btn");
