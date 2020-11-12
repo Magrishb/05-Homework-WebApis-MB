4e9494c4caf1e5862508085410de3d410902ea41
Magrishb <bmagrish@gmail.com>
2020-11-11 18:37:07 -0600

    Listed all the initial variables in js

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,3 +1,9 @@
 // List all the variables.
-
+var questions = document.getElementById("questions");
+var timer = document.getElementById("time");
+var choices = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initial = document.getElementById("initials");
+var feedback = document.getElementById("feedback");
 
